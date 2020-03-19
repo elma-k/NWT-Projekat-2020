527c8c7d7a05580bcece3efcf1fd63cf7d1794dc
Amina Fajić <37187803+aminaanaan@users.noreply.github.com>
2020-03-19 12:37:47 +0100

    Create Podaci.java

---
+++ src/main/java/com/hotel/ena/models/Podaci.java
@@ -0,0 +1,111 @@
+package com.hotel.ena.models;
+
+
+
+
+
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+
+
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+
+
+
+import org.springframework.context.event.EventListener;
+
+
+
+import org.springframework.stereotype.Component;
+
+
+
+
+
+
+
+import java.util.ArrayList;
+
+
+
+import java.util.List;
+
+
+
+
+
+
+
+@Component
+
+
+
+public class Podaci {
+
+
+
+
+
+
+
+    private RacunRepository racunRepozitorij;
+
+
+    @Autowired
+
+
+
+    public Podaci(RacunRepository racunRepozitorij) {
+
+
+
+        this.racunRepozitorij = racunRepozitorij;
+
+
+
+    }
+
+
+
+
+
+
+
+    @EventListener
+
+
+
+    public void dodaj (ApplicationReadyEvent event){
+
+
+
+        dodajRacune();
+    }
+
+
+
+
+
+
+
+    private void dodajRacune() {
+
+
+
+        RacunEntity racun = new RacunEntity();
+
+        
+
+       racun.setCreatedBy("Amina");
+       racun.setCost(1000);
+       racun.setPaid(false);
+       racun.setCreated("2020-09-08");
+       
+        racunRepozitorij.save(racun);
+
+        }
+
+      
+
